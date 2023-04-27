d49527b90dd5282be12fc2c0ceb7a2136cd85ee1
Manuel Baumgartner <manu.baumi@outlook.com>
2023-04-27 21:22:41 +0200

    Add color to text

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const color = randomColor({
 
 const log = console.log;
 chalk.level = 1;
-log(chalk.yellow('Welcome to the app'));
 
 console.log(chalk.hex(color).bold('###############################'));
 console.log(chalk.hex(color).bold('###############################'));
@@ -22,3 +21,18 @@ console.log(chalk.hex(color).bold('#####                     #####'));
 console.log(chalk.hex(color).bold('###############################'));
 console.log(chalk.hex(color).bold('###############################'));
 console.log(chalk.hex(color).bold('###############################'));
+
+
+//9:11:43 PM:   └─┬ eslint-plugin-flowtype 8.0.3
+//9:11:43 PM:     ├── ✕ missing peer @babel/plugin-syntax-flow@^7.14.5
+//9:11:43 PM:     └── ✕ missing peer @babel/plugin-transform-react-jsx@^7.14.9
+//9:11:43 PM: Peer dependencies that should be installed:
+//9:11:43 PM:   @babel/core@>=7.11.0
+////9:11:43 PM:   @babel/plugin-syntax-flow@^7.14.5
+//9:11:43 PM:   @babel/plugin-transform-react-jsx@^7.14.9
+//9:11:43 PM: hint: If you want peer dependencies to be automatically installed, add "auto-install-peers=true" to an .npmrc file at the root of your project.
+//9:11:43 PM: hint: If you don't want pnpm to fail on peer dependency issues, add "strict-peer-dependencies=false" to an .npmrc file at the root of your project.
+//9:11:44 PM: Error during pnpm install
+//9:11:44 PM: Build was terminated: dependency_installation script returned non-zero exit code: 1
+//9:11:44 PM: Failing build: Failed to install dependencies
+//9:11:44 PM: Finished processing build request in 22.892s
